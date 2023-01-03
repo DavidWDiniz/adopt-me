4008c820ce10b63633bdf53b37453e5c1daa77e1
david <davidwdiniz@gmail.com>
2023-01-03 11:16:16 -0300

    Code splitting and SSR with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Details from "./Details";
+import { Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
+      suspense: true,
     },
   },
 });
@@ -18,9 +19,15 @@ const App = () => {
   const adoptedPet = useState(null);
 
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
+    <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">🌀</h2>
+            </div>
+          }
+        >
           <header>
             <Link to="/">Adopte me!</Link>
           </header>
@@ -28,12 +35,10 @@ const App = () => {
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
+        </Suspense>
+      </AdoptedPetContext.Provider>
+    </QueryClientProvider>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
